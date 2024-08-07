9513535e1ed13bdccc52580de5feca0f560d78d3
henrique11varela <henrique11varela@gmail.com>
2024-08-07 21:25:35 +0100

    Started structuring project

---
+++ src/components/shared/ButtonElement.vue
@@ -0,0 +1,20 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  background-color: var(--primary);
+  color: var(--light);
+  padding: 0.7em;
+  border-radius: 0.5em;
+  width: 100%;
+  height: 100%;
+  font-weight: 700;
+  font-size: 1.1em;
+  font-weight: 700;
+  font-size: 1.1em;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <router-link :to="{ name: 'about' }">
+      <ButtonElement>About</ButtonElement>
+    </router-link>
   </main>
 </template>
+
+<script setup lang="ts">
+import ButtonElement from '@/components/shared/ButtonElement.vue'
+</script>
